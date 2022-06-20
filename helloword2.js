94ee973800c4cf07285d37de8b6334055eab21a7
Ati-netauto <100962475+Ati-netauto@users.noreply.github.com>
2022-06-21 00:42:20 +0100

    Update helloword2.js
    updated all code

--- helloword2.js
+++ helloword2.js
@@ -1,14 +1,10 @@
-const http = require('http');
-
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Send back a response and end the connection
-    res.end('Hello World!\n');
-});
-
-// Start the server on port 3000
-app.listen(80, '127.0.0.1');
-console.log('Node server running on port 80');
+var http = require('http');  
+//create a server object:  
+  
+http.createServer(function (req, res) {  
+    res.write('Hello World!'); //write a response to the client  
+    res.end(); //end the response  
+}).listen(5000); //the server object listens on port 8080   
+  
+// Console will print the message  
+console.log('Server running at 5000');
